942ed0263ba959f2ec68bf4d13da6f05f6cf8b00
Mary Lark <mary@marylark.com>
2018-11-25 00:21:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector) {
+  var element;
+  //return first element that matches
+  element = document.querySelector(selector);
+  return element;
+}
+
+function nestedTarget() {
+  //pulls a .target out of #nestedTarget
+  //#nested and .target are div's.
+  var target;
+  return target;
+}
+
+function increaseRankBy(n) {
+  //increases theranks in all of the .ranked-list s by n
+  //use parseInt()
+  
+}
+
+function deepestChild() {
+  //pulls out the most deeply nested child element from div#grand-node//iterate over elements and call querySelector() and querySelectorAll() on them
+  //does not need to be totally generic
+  //i.e. : assume there are no sibling nodes
+  // recall breadth-first search here
+  
+}
 
